3c1df6c0f5ea6b547abbfc70eebc73ed09b5cf79
Agata Jarmoszuk <agnes365@vp.pl>
2021-11-11 21:00:59 +0100

    Add function generateTags

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';
 
 function titleClickHandler(event) {
   event.preventDefault();
@@ -66,9 +67,9 @@ function generateTitleLinks(customSelector = '') {
     html = html + link;
   }
   titleList.insertAdjacentHTML('beforeend', html);
-  
+
   const links = document.querySelectorAll(optTitleListSelector + ' a');
-  
+
   for (let link of links) {
     link.addEventListener('click', titleClickHandler);
   }
@@ -217,3 +218,54 @@ function addClickListenersToAuthors() {
   }
 }
 addClickListenersToAuthors();
+
+function generateTags() {
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+
+    /* find tags wrapper */
+    const tagWrapper = article.querySelector(optArticleTagsSelector);
+
+    /* make html variable with empty string */
+    let html = '';
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+
+      /* generate HTML of the link */
+      const li = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+      /* add generated code to html variable */
+      html = html + li;
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if (allTags.indexOf(linkHTML) == -1) {
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
+
+      /* END LOOP: for each tag */
+    }
+
+    /* insert HTML of all the links into the tags wrapper */
+    tagWrapper.insertAdjacentHTML('beforeend', html);
+    
+    /* END LOOP: for every article: */
+  }
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
+}
